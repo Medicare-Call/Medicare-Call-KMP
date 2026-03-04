3f79166041b1057680721b92e39e3df4107e352e
ProtossManse <ingan2473@gmail.com>
2026-03-05 04:43:02 +0900

    Fix: 안드로이드에서 FcmRepository 관련해서 실행되지 않던 문제 수정

--- composeApp/src/androidMain/kotlin/com/konkuk/medicarecall/data/di/LocalModule.kt
+++ composeApp/src/androidMain/kotlin/com/konkuk/medicarecall/data/di/LocalModule.kt
@@ -3,8 +3,12 @@ package com.konkuk.medicarecall.data.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
+import com.konkuk.medicarecall.data.api.fcm.FcmUpdateService
+import com.konkuk.medicarecall.data.api.fcm.FcmValidationService
 import com.konkuk.medicarecall.data.model.ElderIds
 import com.konkuk.medicarecall.data.model.Token
+import com.konkuk.medicarecall.data.repository.FcmRepository
+import com.konkuk.medicarecall.data.repositoryimpl.FcmRepositoryImpl
 import com.konkuk.medicarecall.data.util.ElderIdsSerializer
 import com.konkuk.medicarecall.data.util.TokenSerializer
 import org.koin.core.annotation.Module
@@ -33,5 +37,18 @@ class LocalModule {
     @Single
     fun provideElderIdDataStore(context: Context): DataStore<ElderIds> {
         return context.elderIdDataStore
+    }
+
+    @Single
+    fun provideFcmRepository(
+        context: Context,
+        fcmValidationService: FcmValidationService,
+        fcmUpdateService: FcmUpdateService,
+    ): FcmRepository {
+        return FcmRepositoryImpl(
+            context = context,
+            fcmValidationService = fcmValidationService,
+            fcmUpdateService = fcmUpdateService,
+        )
     }
 }
