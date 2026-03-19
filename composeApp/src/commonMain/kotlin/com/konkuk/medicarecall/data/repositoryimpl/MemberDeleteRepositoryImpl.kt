580decb9e761839677de05de1df3f713a3515fd0
ProtossManse <ingan2473@gmail.com>
2026-03-19 20:53:57 +0900

    Update MemberDeleteRepositoryImpl.kt

--- composeApp/src/commonMain/kotlin/com/konkuk/medicarecall/data/repositoryimpl/MemberDeleteRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/konkuk/medicarecall/data/repositoryimpl/MemberDeleteRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.konkuk.medicarecall.data.repositoryimpl
 
 import com.konkuk.medicarecall.data.api.member.MemberDeleteService
 import com.konkuk.medicarecall.data.repository.MemberDeleteRepository
+import com.konkuk.medicarecall.data.util.handleNullableResponse
 import org.koin.core.annotation.Single
 
 @Single
@@ -9,6 +10,6 @@ class MemberDeleteRepositoryImpl(
     private val memberDeleteService: MemberDeleteService,
 ) : MemberDeleteRepository {
     override suspend fun deleteMember(): Result<Unit> = runCatching {
-        memberDeleteService.deleteMember()
+        memberDeleteService.deleteMember().handleNullableResponse()
     }
 }
