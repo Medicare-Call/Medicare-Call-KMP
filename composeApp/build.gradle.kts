034eab4ba467c65bb80f7f79a9d89461c08d2490
librawish808 <sophiadiary842@gmail.com>
2026-03-24 23:16:35 +0900

    Chore: 아이콘 및 의존성 추가(프리뷰) #15

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,7 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
+            implementation("org.jetbrains.compose.ui:ui-tooling-preview:1.10.1")
 
             // Navigation (KMP)
             implementation(libs.navigation.compose)
@@ -157,6 +158,9 @@ compose.resources {
 dependencies {
     // Detekt formatting plugin
     detektPlugins(libs.detekt.formatting)
+
+    // Compose preview tooling for Android Studio
+    add("debugImplementation", "org.jetbrains.compose.ui:ui-tooling:1.10.1")
 
     // Firebase (Android only)
     add("androidMainImplementation", platform(libs.firebase.bom))
